a20a4dc50a90b39f757e84ea2bc9e98785104dde
dannyelgjl <dannyelgjl@gmail.com>
2020-10-26 20:11:47 -0300

    Estilizando página de repositórios

--- src/Pages/Repository/index.tsx
+++ src/Pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import githubImage from '../../assets/logo-github.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,9 +13,52 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+  console.log(params);
 
   return (
-    <h1>Repository: {params.repository} </h1>
+    <>
+      <Header>
+        <img src={githubImage} alt="Github Explore" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars1.githubusercontent.com/u/54491980?s=460&u=5457192f7674845b14a107f7791033cfcbabb036&v=4" alt="Me" />
+          <div>
+            <strong>Daniel/Detran</strong>
+            <p>Testando</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>60</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={"teste"}>
+          <div>
+            <strong>teste</strong>
+            <p>testesdasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 }
 
