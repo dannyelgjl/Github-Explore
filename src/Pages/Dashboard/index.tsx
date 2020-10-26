aae7c0a5d06bca86dbb9a7cf1fcdb3482961856b
dannyelgjl <dannyelgjl@gmail.com>
2020-10-26 17:39:26 -0300

    salvando os dados no localstorage

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import githubImage from '../../assets/logo-github.svg';
@@ -22,12 +22,25 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
   const [inputError, setInputError] = useState('')
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplore:repositories');
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    } else {
+      return [];
+    }
+  });
+
+
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplore:repositories',
+      JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
-
-
 
     if (!newRepo) {
       setInputError('Digite o auto/nome do repositório');
